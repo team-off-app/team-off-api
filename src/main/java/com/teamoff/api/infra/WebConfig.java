0e16874362218233f3b62212dded77a89893af98
matheuspegorari <pegorari42@gmail.com>
2023-12-25 23:03:32 -0300

    fix: cors

--- src/main/java/com/teamoff/api/infra/WebConfig.java
+++ src/main/java/com/teamoff/api/infra/WebConfig.java
@@ -9,6 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
