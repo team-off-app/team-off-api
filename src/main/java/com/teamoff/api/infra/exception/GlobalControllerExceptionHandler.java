9f379c9637b9f8c45c1673581f1fe2c39c03e665
matheuspegorari <pegorari42@gmail.com>
2024-01-09 21:31:27 -0300

    feat: treated badCredentialsException, plus some refactoring in GlobalControllerExceptionHandler

--- src/main/java/com/teamoff/api/infra/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/teamoff/api/infra/exception/GlobalControllerExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,12 +20,9 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
-
-    private final String REGEX_ERROR = "\\[ERROR:.*?\\]\\.";
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -36,55 +34,52 @@ public class GlobalControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentTypeMismatchException.class})
-    public Map<String, String> handleMethodArgumentTypeMismatchException(
-            MethodArgumentTypeMismatchException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put(ex.getName(), ex.getMessage());
-        return error;
+    public Map<String, String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        return showError(ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({ResourceNotFoundException.class})
-    public Map<String, String> handleUserNotFoundException(
-            ResourceNotFoundException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", ex.getMessage());
-        return error;
+    public Map<String, String> handleUserNotFoundException(ResourceNotFoundException ex) {
+        return showError(ex.getMessage());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Object> handleSqlException (DataIntegrityViolationException ex){
+    public ResponseEntity<Object> handleSqlException() {
         return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler({EntityNotFoundException.class, NoHandlerFoundException.class})
-    public ResponseEntity<Object> tratarErro404() {
+    public ResponseEntity<Object> handleNotFound() {
         return ResponseEntity.notFound().build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({HttpMessageNotReadableException.class})
-    public Map<String, String> handleMessageNotReadableException(
-            HttpMessageNotReadableException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", ex.getMessage());
-        return error;
+    public Map<String, String> handleMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return showError(ex.getMessage());
     }
 
     @ExceptionHandler({ResponseStatusException.class})
-    public ResponseEntity<Map<String, String>> handleResponseStatusException(
-            ResponseStatusException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", ex.getReason());
-        return new ResponseEntity<>(error, ex.getStatusCode());
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(showError(ex.getReason()), ex.getStatusCode());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler({BadCredentialsException.class})
+    public Map<String, String> handleBadCredentials(Exception ex) {
+        return showError(String.format(ex.getMessage()));
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
-    public Map<String, String> handleException(
-            Exception ex) {
+    public Map<String, String> handleException(Exception ex) {
+        return showError(String.format("%s - %s", ex.getClass().getName(), ex.getMessage()));
+    }
+
+    private Map<String, String> showError(String message) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", ex.getMessage());
+        error.put("error", message);
         return error;
     }
 
